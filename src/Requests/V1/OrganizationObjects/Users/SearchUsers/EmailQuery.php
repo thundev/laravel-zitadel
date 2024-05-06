79ff01d5dc62f4c5d57ceb3dfcdb311acd5ddab8
m.lebedev <vk384d@gmail.com>
2024-05-06 12:05:51 +0300

    Update search users

--- src/Requests/V1/OrganizationObjects/Users/SearchUsers/EmailQuery.php
+++ src/Requests/V1/OrganizationObjects/Users/SearchUsers/EmailQuery.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Thundev\Zitadel\Requests\V1\OrganizationObjects\Users\SearchUsers;
 
+use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
+#[MapInputName(SnakeCaseMapper::class)]
 class EmailQuery extends Data
 {
     public function __construct(
