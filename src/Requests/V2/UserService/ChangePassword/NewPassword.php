20eec76af8507272dc937489d4dd3240e611244e
m.lebedev <vk384d@gmail.com>
2024-06-18 15:34:52 +0300

    Update change password request

--- src/Requests/V2/UserService/ChangePassword/NewPassword.php
+++ src/Requests/V2/UserService/ChangePassword/NewPassword.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Thundev\Zitadel\Requests\V2\UserService\ChangePassword;
 
+use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
+#[MapInputName(SnakeCaseMapper::class)]
 class NewPassword extends Data
 {
     public function __construct(
